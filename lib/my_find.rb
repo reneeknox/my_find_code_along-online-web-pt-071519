d00a6f54c9aca483324ef194fd8f7f1e11720c91
Renee France <renee5.0@outlook.com>
2019-07-28 22:10:27 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-  if yield(collection[i])
-    return collection[i]
+  if return collection[i]yield(collection[i])
+    
   i = i + 1
   end
 end
